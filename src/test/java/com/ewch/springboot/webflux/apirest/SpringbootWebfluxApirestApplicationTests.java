7e30ac98044e07e2f4920a9888f925dfa676818e
eimer-castro-g <eimer.castro@globant.com>
2020-10-07 09:57:36 -0500

    Implementing test to getAllProducts endpoint.

--- src/test/java/com/ewch/springboot/webflux/apirest/SpringbootWebfluxApirestApplicationTests.java
+++ src/test/java/com/ewch/springboot/webflux/apirest/SpringbootWebfluxApirestApplicationTests.java
@@ -1,13 +1,29 @@
 package com.ewch.springboot.webflux.apirest;
 
+import com.ewch.springboot.webflux.apirest.model.document.Product;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 class SpringbootWebfluxApirestApplicationTests {
 
+	@Autowired
+	private WebTestClient webTestClient;
+
 	@Test
-	void contextLoads() {
+	public void getAllProductsTest() {
+		webTestClient.get()
+		.uri("/api/v2/products")
+		.accept(MediaType.APPLICATION_JSON_UTF8)
+		.exchange()
+		.expectStatus().isOk()
+		.expectHeader().contentType(MediaType.APPLICATION_JSON_UTF8)
+		.expectBodyList(Product.class)
+		.hasSize(9);
 	}
 
 }
