0f70c460cb266d28033c395c97999fc18bd62584
eimer-castro-g <eimer.castro@globant.com>
2020-10-07 17:01:54 -0500

    Implementing @AutoconfigurationWebTestClient with WebEnvironment.MOCK to use Mock and don't start Netty Server.

--- src/test/java/com/ewch/springboot/webflux/apirest/SpringbootWebfluxApirestApplicationTests.java
+++ src/test/java/com/ewch/springboot/webflux/apirest/SpringbootWebfluxApirestApplicationTests.java
@@ -11,6 +11,7 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.core.ParameterizedTypeReference;
@@ -18,7 +19,8 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Mono;
 
-@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@AutoConfigureWebTestClient
+@SpringBootTest(webEnvironment = WebEnvironment.MOCK)
 class SpringbootWebfluxApirestApplicationTests {
 
 	@Value("${config.base.endpoint}")
