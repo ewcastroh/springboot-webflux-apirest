dc96f57544e2dbbb927f46e00f9cf876b630862b
eimer-castro-g <eimer.castro@globant.com>
2020-10-09 08:26:09 -0500

    Refactoring POST route to upload pictures to a product.

--- src/main/java/com/ewch/springboot/webflux/apirest/config/RouterFunctionConfig.java
+++ src/main/java/com/ewch/springboot/webflux/apirest/config/RouterFunctionConfig.java
@@ -19,7 +19,7 @@ public class RouterFunctionConfig {
 			.andRoute(RequestPredicates.POST("/api/v2/products"), productHandler::createProduct)
 			.andRoute(RequestPredicates.PUT("/api/v2/products/{id}"), productHandler::updateProduct)
 			.andRoute(RequestPredicates.DELETE("/api/v2/products/{id}"), productHandler::deleteProduct)
-			.andRoute(RequestPredicates.POST("/api/v2/uploads/picture/{id}"), productHandler::uploadPicture)
+			.andRoute(RequestPredicates.POST("/api/v2/products/uploads/picture/{id}"), productHandler::uploadPicture)
 			.andRoute(RequestPredicates.POST("/api/v2/products/upload/picture"), productHandler::createProductPicture);
 	}
 }
